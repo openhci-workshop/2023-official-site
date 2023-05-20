b0ef72d13198625a5bb9e9d1908604cba774da40
Derek Lu <derekdylu@gmail.com>
2023-05-21 02:13:32 +0800

    fix: env var by showing it

--- app/submission/page.jsx
+++ app/submission/page.jsx
@@ -25,7 +25,8 @@ const aldrich = Aldrich({
 });
 
 async function fetchSubmissionContent() {
-	const response = await fetch(`https://openhci-2023.netlify.app/api/submission`, {
+	console.log("~~~~~~~~~~~~~~~~~~~~~~~showing env var~~~~~~~~~~~~~~~~~~~~~~~", process.env.NEXT_PUBLIC_BASE_FETCH_URL)
+	const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_FETCH_URL}/api/submission`, {
 		next: {
 			revalidate: 60,
 		},
