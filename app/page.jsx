02b3fd841416bd9dbc1ec0aae4a14d31fdb5e05c
Derek Lu <derekdylu@gmail.com>
2023-06-23 19:19:43 +0800

    fix: og222

--- app/page.jsx
+++ app/page.jsx
@@ -17,8 +17,24 @@ import historywork2 from '../public/history_work2.png';
 
 export const metadata = {
 	title: 'OPENHCI 2023',
-	description: 'OPENHCI 2023 Home Page',
+	description:
+		'OPENHCI 2023 是一個專門推廣人機互動學門及促進垮領域合作的密集工作坊。',
 	keywords: '',
+	openGraph: {
+		title: 'OPENHCI 2023',
+		description:
+			'OPENHCI 2023',
+		url: process.env.NEXT_PUBLIC_BASE_FETCH_URL,
+		siteName: 'OPENHCI 2023 是一個專門推廣人機互動學門及促進垮領域合作的密集工作坊。',
+		images: [
+			{
+				url: `${process.env.NEXT_PUBLIC_BASE_FETCH_URL}/og.png`,
+				width: 1200,
+				height: 630,
+			},
+		],
+		type: 'website',
+	},
 };
 
 const notoSansTC = Noto_Sans_TC({
