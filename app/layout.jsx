762fb48ef9da01e858d6c8e96697584a6d86681c
Derek Lu <derekdylu@gmail.com>
2023-06-23 19:16:21 +0800

    fix: og

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,25 +9,25 @@ const notoSansTC = Noto_Sans_TC({
 });
 
 export const metadata = {
-	// title: 'OPENHCI 2023',
-	// description:
-	// 	'OPENHCI 2023 是一個專門推廣人機互動學門及促進垮領域合作的密集工作坊。',
-	// keywords: '',
-	// openGraph: {
-	// 	title: 'OPENHCI 2023',
-	// 	description:
-	// 		'OPENHCI 2023',
-	// 	url: process.env.NEXT_PUBLIC_BASE_FETCH_URL,
-	// 	siteName: 'OPENHCI 2023 是一個專門推廣人機互動學門及促進垮領域合作的密集工作坊。',
-	// 	images: [
-	// 		{
-	// 			url: `${process.env.NEXT_PUBLIC_BASE_FETCH_URL}/og.png`,
-	// 			width: 1200,
-	// 			height: 630,
-	// 		},
-	// 	],
-	// 	type: 'website',
-	// },
+	title: 'OPENHCI 2023',
+	description:
+		'OPENHCI 2023 是一個專門推廣人機互動學門及促進垮領域合作的密集工作坊。',
+	keywords: '',
+	openGraph: {
+		title: 'OPENHCI 2023',
+		description:
+			'OPENHCI 2023',
+		url: process.env.NEXT_PUBLIC_BASE_FETCH_URL,
+		siteName: 'OPENHCI 2023 是一個專門推廣人機互動學門及促進垮領域合作的密集工作坊。',
+		images: [
+			{
+				url: `${process.env.NEXT_PUBLIC_BASE_FETCH_URL}/og.png`,
+				width: 1200,
+				height: 630,
+			},
+		],
+		type: 'website',
+	},
 };
 
 export default function RootLayout({ children }) {
