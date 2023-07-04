8d5ef9a74d6f8d43ec46fc57ce51cc319308bf43
Matthew <matthew6303@gmail.com>
2023-07-04 22:31:58 +0800

    feat: title fade in effect

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,7 @@
 import { Noto_Sans_TC } from 'next/font/google';
-import Script from 'next/script'
+import Script from 'next/script';
+
+import Providers from '@/components/organisms/Providers';
 
 import './globals.css';
 
@@ -11,8 +13,7 @@ const notoSansTC = Noto_Sans_TC({
 export const metadata = {
 	openGraph: {
 		title: 'OPENHCI 2023',
-		description:
-			'OPENHCI 2023',
+		description: 'OPENHCI 2023',
 		url: process.env.NEXT_PUBLIC_BASE_FETCH_URL,
 		siteName: 'OPENHCI 2023',
 		images: [
@@ -25,8 +26,7 @@ export const metadata = {
 		type: 'website',
 	},
 	title: 'OPENHCI 2023',
-	description:
-		'OPENHCI 2023 是一個專門推廣人機互動學門及促進垮領域合作的密集工作坊。',
+	description: 'OPENHCI 2023 是一個專門推廣人機互動學門及促進垮領域合作的密集工作坊。',
 	keywords: '',
 };
 
@@ -34,18 +34,23 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
 			<body className={notoSansTC.className} suppressHydrationWarning>
-
-				<Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} id="gasrc"/>
+				<Script
+					strategy="lazyOnload"
+					src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+					id="gasrc"
+				/>
 				<Script strategy="lazyOnload" id="gacode">
-						{`
+					{`
 							window.dataLayer = window.dataLayer || [];
 							function gtag(){dataLayer.push(arguments);}
 							gtag('js', new Date());
 							gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
 						`}
 				</Script>
-				
-				<main className="pt-16 md:pt-20 overflow-hidden">{children}</main>
+
+				<main className="pt-16 md:pt-20 overflow-hidden">
+					<Providers>{children}</Providers>
+				</main>
 			</body>
 		</html>
 	);
